b6d805a2c2158c137227c4b7ddfe1def221eb154
chenCB931223 <42188252+chenCB931223@users.noreply.github.com>
2018-09-14 06:52:19 +0800

    Update index2.js

--- js/index2.js
+++ js/index2.js
@@ -2,23 +2,23 @@ $(document).ready(function(){
     var slide1 = new Slide('#box1',['images/muke1.jpg','images/muke2.jpg','images/muke3.jpg','images/muke4.jpg'])
 
     var $pre = $('#btn1');
-    $pre.click(function(){
+    $pre.stop(true,true).click(function(){
         slide1.go(-1);
     });
     $pre.mouseover(function(){
-        slide1.stop();
+        slide1.cease();
     });
     $pre.mouseout(function(){
         slide1.play();
     });
 
     var $next = $('#btn2');
-    $next.click(function(){
+    $next.stop(true,true).click(function(){
         slide1.go(1);
     });
 
     $next.mouseover(function(){
-        slide1.stop();
+        slide1.cease();
     });
 
     $next.mouseout(function(){
@@ -32,7 +32,6 @@ function Slide(box,images,duration){
     var duration = duration || 2000;
     this.duration = duration;
     this.now = 0;
-
     this.timer = null;
     this.init();
 }
@@ -40,17 +39,14 @@ function Slide(box,images,duration){
 Slide.prototype = {
     init : function(){
         this.creat();
-        this.move();
         this.dotClass();
         this.addEven();
         this.play();
     },
     creat : function(){
         var contenBox = $('<ul class="content"></ul>');
-        this.box.append(contenBox);
 
         var control = $('<div class="control"></div>');
-        this.box.append(control);
 
         $.each(this.images,function(i,item){
             var $createImg = '<li><img src="' + item + '"/></li>';
@@ -59,41 +55,31 @@ Slide.prototype = {
             var $createDot = '<span></span>';
             control.append($createDot);
         });
-        var $list = $('.content li:first').clone();
+	    
+	this.box.append(contenBox);
+	this.box.append(control);
+	    
+        var $list = $('.content li').clone();
         contenBox.append($list);
-        contenBox.css('width',$list.width() * (this.len + 1));
+        contenBox.css('width',$list.width() * (this.len * 2));
     },
-
-    move : function(){
-        $list = $('.content');
-        $item = $('.content li');
-        $list.css('left',-$item.width() * this.now + 'px');
-    },
-
+	
     dotClass : function(){
-        var $dot = $('.control span');        
-        $.each($dot,function(i,j){
-           if($dot[i].className == 'on'){
-            $dot[i].className = ' ';
-           }         
-        })
-        $dot[this.now % this.len].className = 'on';
+	var $dot = $('.control').find('span');
+        $dot.eq(this.now % this.len).addClass('on').siblings('span').removeClass('on');
     },
 
     go : function(step){
-        var $liWidth = $('.content li').eq(1);
+        var $liWidth = $('.content li').eq(1).width();
         this.now += step;
-		$(".content").animate({'left':-$liWidth.width() * this.now});
+	$(".content").animate({'left':-$liWidth * this.now});
         if(this.now == 0){
-
-           $(".content").animate({'left':-$liWidth.width() * this.len},0);
-           this.now = this.len ;
+           $(".content").animate({'left':-$liWidth * this.len + 1},0);
+           this.now = this.len;
         }
-        if(this.now == this.len){
-            $(".content").animate({'left':0},0);
-            $list.css('left','0');
-
-            this.now = 0;
+        if(this.now == this.len + 1){
+            $(".content").animate({'left':-$liWidth},0);
+            this.now = 1;
         }
         this.dotClass();
     },
@@ -103,18 +89,16 @@ Slide.prototype = {
         
         $.each($dots,function(index,elment){
             this.index = index;
-            $('.control').on('click','span',function(){
-                
-            if(this.index == _this.now) return;
-            _this.now = this.index;
-
-            _this.dotClass();
-            _this.move();
+            $dots.on('click',function(){     
+            	if(this.index == _this.now) return;
+            	_this.now = this.index;
+		$('.content').css('left', -$('.content').find('li').eq(1).width() * _this.now + 'px');
+            _	this.dotClass();
             })
         });
 
         _this.box.mouseover(function(){
-            _this.stop();
+            _this.cease();
         });
 
         _this.box.mouseout(function(){
@@ -128,7 +112,7 @@ Slide.prototype = {
             _this.go(1);
         },_this.duration);
     },
-    stop : function(){
+    cease : function(){
         clearInterval(this.timer);
     }
 }
